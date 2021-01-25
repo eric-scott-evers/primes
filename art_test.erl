b4af249d1b1c0953a8a3f6170e71eca01120bc0c
Eric Scott Evers <47706463+eric-scott-evers@users.noreply.github.com>
2021-01-24 22:25:02 -0500

    Update art_test.erl
    this will test art.erl

--- art_test.erl
+++ art_test.erl
@@ -1,39 +1,46 @@
-7> art:init(). 
+-module(art_test).
+
+%  need to complile art.erl first with c(art) 
+
+art:init(). 
 %   {atomic,ok}
  
-%   6> art:insert(1,"Dali","The Ghost of Vermeer").
+art:insert(1,"Dali","The Ghost of Vermeer").
 %   {atomic,ok}
   
-%   7> art:select(1).
+art:select(1).
 %   "Dali" "The Ghost of Vermeer"
 %   ok
   
-%   8> art:insert(2,"Dali","The Persistence of Memory").
+art:insert(2,"Dali","The Persistence of Memory").
 %   {atomic,ok}
    
-%   9> art:select(2).
+art:select(2).
 %   "Dali" "The Persistence of Memory"
 %   ok
    
-%   10> art:select(1).
+art:select(1).
 %   "Dali" "The Ghost of Vermeer"
 %   ok 
  
-%   25> art:insert(3,"Vermeer", "Girl With Pearl Earring").
+art:insert(3,"Vermeer", "Girl With Pearl Earring").
 %   {atomic,ok}
  
-%   26> art:select_some("Dali").
+art:select_some("Dali").
 %   [{painting,1,"Dali","The Ghost of Vermeer"},
 %   {painting,2,"Dali","The Persistence of Memory"}]
  
-%   27> art:select_all().
+art:select_all().
 %   {atomic,[{painting,1,"Dali","The Ghost of Vermeer"},
 %               {painting,2,"Dali","The Persistence of Memory"},
 %               {painting,3,"Vermeer","Girl With Pearl Earring"}]}
 
 %---to run a new session after restarting erlang---
  
-%   2> art:init().
+art:init().
 %   {aborted,{already_exists,painting}}
 
-%   3> art:select_search("Vermeer").
+art:select_search("Vermeer").
+%      {atomic,[{painting,1,"Dali","The Ghost of Vermeer"},
+%               {painting,3,"Vermeer","Girl With Pearl Earring"}]}
+
