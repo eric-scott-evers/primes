352ea0e08f0ed6893cd009d27c013c4fb2ac10e9
eric-scott-evers <mindserfer@yahoo.com>
2021-01-25 13:11:07 -0500

    add files

--- art_test.erl
+++ art_test.erl
@@ -13,28 +13,38 @@ art:insert(1,"Dali","The Ghost of Vermeer"),
 %   {atomic,ok}
   
 A = art:select(1),
+AA = "art:select(1)",
+
 %   "Dali" "The Ghost of Vermeer"
 %   ok
   
 art:insert(2,"Dali","The Persistence of Memory"),
 %   {atomic,ok}
    
-B = art:select(2),
+B  = art:select(2),
+BB = "art:select(2)",
+
 %   "Dali" "The Persistence of Memory"
 %   ok
    
-C = art:select(1),
+C  = art:select(1),
+CC = "art:select(1)",
+
 %   "Dali" "The Ghost of Vermeer"
 %   ok 
  
 art:insert(3,"Vermeer", "Girl With Pearl Earring"),
 %   {atomic,ok}
  
-D = art:select_some("Dali"),
+D  = art:select_some("Dali"),
+DD = "art:select_some(\"Dali\")",
+
 %   [{painting,1,"Dali","The Ghost of Vermeer"},
 %   {painting,2,"Dali","The Persistence of Memory"}]
  
-E = art:select_all(),
+E  = art:select_all(),
+EE = "art:select_all()",
+ 
 %   {atomic,[{painting,1,"Dali","The Ghost of Vermeer"},
 %               {painting,2,"Dali","The Persistence of Memory"},
 %               {painting,3,"Vermeer","Girl With Pearl Earring"}]}
@@ -44,9 +54,11 @@ E = art:select_all(),
 % art:init(),
 %   {aborted,{already_exists,painting}}
 
-F = art:select_search("Vermeer"),
+F  = art:select_search("Vermeer"),
+FF = "art:select_search(\"Vermeer\")",
+
 %      {atomic,[{painting,1,"Dali","The Ghost of Vermeer"},
 %               {painting,3,"Vermeer","Girl With Pearl Earring"}]}
  
 %  all outputs 
-[A, B, C, D, E, F].
+[AA, A, BB, B, CC, C, DD, D, EE, E, FF, F].
